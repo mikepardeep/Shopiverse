2f9a15f9db43bed8aa798db49f0f8a711008ec40
pardeep <mikepardeep@gmail.com>
2022-06-28 10:59:10 +0800

    Done initial setup of stripe payment gateway

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -5,6 +5,10 @@ const Order = require('../models/order.model')
 //import user models
 const User = require('../models/user.model')
 
+//import stripe payment
+const stripe = require('stripe')('sk_test_51LFKQ6JTkEOTF98q75RbcLkU3TDSqBgi9nR0AeuDDkkNLf6lAKEgItxwBC0aqMCtpQAMeapP51wIIy4OyyttHoOC00zCEPoTCY');
+
+
 //get Order
 async function getOrders(req, res, next) {
     try {
@@ -46,13 +50,47 @@ async function addOrder(req,res, next) {
     //clear the cart session
     req.session.cart = null;
 
-    //redirect to order page
-    res.redirect('/orders');
+    //logic for stripe
 
+    //create session for stripe checkout
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price_data : {
+            currency: 'RM',
+            product_data: {
+              name: 'Dummy'
+            },
+            unit_amount_decimal : 10.99
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `localhost:3000/orders/success`,
+      cancel_url: `localhost:3000/orders/cancel`,
+    });
+  
+    res.redirect(303, session.url);
+
+    // //redirect to order page
+    // res.redirect('/orders');
 
 }
+
+function getSuccess(req,res){
+  res.render('customer/orders/success')
+}
+
+function getFailure(req,res){
+  res.render('customer/orders/failure')
+}
+
 
 module.exports = {
     addOrder:addOrder,
     getOrders:getOrders,
+    getSuccess: getSuccess,
+    getFailure: getFailure
 }
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -16,5 +16,10 @@ router.post('/', ordersController.addOrder);
 //get route
 router.get('/', ordersController.getOrders); // /orders
 
+//payment success and failure route
+router.get('/success', ordersController.getSuccess);
+
+router.get('/failure', ordersController.getFailure);
+
 //export the router available global
 module.exports = router;
